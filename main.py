b0c154cc0ce1362ba5e805a03341ef44c1450b7d
skyfireitdiy <skyfireitdiy@hotmail.com>
2020-07-22 21:54:24 +0800

    调整日志位置

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ if __name__ == '__main__':
     os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
     app = Flask(__name__)
     app.secret_key = uuid.uuid1().hex
+    config = load_config("config.json")
+    os.makedirs(config["local_file_save_path"], exist_ok=True)
+    os.makedirs(config["log_file_path"], exist_ok=True)
 
     formatter = logging.Formatter(
         "[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s")
     handler = TimedRotatingFileHandler(
-        "zn.log", when="D", interval=1, backupCount=15,
+        os.path.join(config["log_file_path"], "zn.log"), when="D", interval=1, backupCount=15,
         encoding="UTF-8", delay=False, utc=True)
     app.logger.addHandler(handler)
     handler.setFormatter(formatter)
 
-    config = load_config("config.json")
-    os.makedirs(config["local_file_save_path"], exist_ok=True)
+    
     init_service(config)
     app.register_blueprint(api, url_prefix='/api')
     app.run(host='0.0.0.0', port=config["port"], debug=False)
